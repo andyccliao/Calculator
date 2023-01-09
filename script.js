b4b585039464232960054e3dc225f12e8c979193
andyccliao <andyccliao@gmail.com>
2023-01-09 14:55:51 -0800

    Add basic calculator functions.

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(x, y) {
+    return x + y;
+}
+function subtract(x, y) {
+    return x - y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(op, x, y) {
+    switch (op) {
+        case "+":
+            add(x, y);
+            break;
+        case "-":
+            subtract(x, y);
+            break;
+        case "*":
+            multiply(x, y);
+            break;
+        case "/":
+            divide(x, y);
+            break;
+        default:
+            alert("Invalid operator!");
+    }
+}
+
