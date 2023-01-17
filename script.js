34daed9e0e72843b7917c3eed2eab26925c7577d
andyccliao <andyccliao@gmail.com>
2023-01-17 15:24:08 -0800

    Start operatorClickCB and operator functionality.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const MAX_TEXT_LENGTH = 20;
-var firstNumber = NaN;
 var currentNumber = NaN;
+var leftOperand = 0;
+var rightOperand = NaN;
+var result = NaN;
+var operator = null;
 
 function add(x, y) {
     return x + y;
@@ -18,23 +21,26 @@ function divide(x, y) {
 function operate(op, x, y) {
     switch (op) {
         case "+":
-            add(x, y);
-            break;
+            return add(x, y);
         case "-":
-            subtract(x, y);
-            break;
+            return subtract(x, y);
         case "*":
-            multiply(x, y);
-            break;
+            return multiply(x, y);
         case "/":
-            divide(x, y);
-            break;
+            return divide(x, y);
         default:
             alert("Invalid operator!");
     }
 }
 
+function operatorClickCB() {
+    
+}
+
 function numberClickCB(e, display) {
+    if (rightOperand === NaN) {
+        rightOperand = 0;
+    }
     if (display.textContent.length <= MAX_TEXT_LENGTH) {
         currentNumber = currentNumber * 10 + +e.target.value;
         display.textContent = currentNumber;
@@ -45,7 +51,16 @@ function numberClickCB(e, display) {
 /* --------- main --------- */
 const displayh1 = document.querySelector(".display h1");
 const numbersContainer = document.querySelector(".numbers");
+const operatorContainer = document.querySelector(".operators");
 numbersContainer.childNodes.forEach((n) => n.addEventListener("click", (x)=>numberClickCB(x, displayh1)));
+operatorContainer.childNodes.forEach((n) => n.addEventListener("click", (x)=>operatorClickCB))
 
-currentNumber = 0;
 
+
+
+/*
+click numbers, add to currentNumber
+press operator, save number to savedNumber, save operator
+enter new number or change operator
+press equals or another operator to calculate, potentially enter another number
+*/
